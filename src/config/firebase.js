44a1c1a60830006f3d4db260f35b8011852ffda6
Cedric Jud <cedric@apt.no>
2020-04-03 20:34:41 +0200

    Config firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZ4YESd1XzBO-wa1D6Txa4hAvrZrsq1hI",
+  authDomain: "mathome-a2bd5.firebaseapp.com",
+  databaseURL: "https://mathome-a2bd5.firebaseio.com",
+  projectId: "mathome-a2bd5",
+  storageBucket: "mathome-a2bd5.appspot.com",
+  messagingSenderId: "401938557843",
+  appId: "1:401938557843:web:a0d64c7622872156535666",
+  measurementId: "G-F9ML7LB38D"
+};
+
+firebase.init(firebaseConfig);
+
+export default firebase;
+
