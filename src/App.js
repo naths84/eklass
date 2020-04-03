7e594acf4d9ea1a7f9f2617a20a68b18bbbb6d13
Cedric Jud <cedric@apt.no>
2020-04-03 21:39:28 +0200

    Add react-hook demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import {Router} from '@reach/router';
+import { useDocumentData, useDocument } from 'react-firebase-hooks/firestore';
+
+import firebase, { db } from './config/firebase';
+
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+// Pages
+import Class from './components/Class/Class';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+
 import './App.css';
 
 function App() {
+  const [value, loading, error] = useDocument(
+    // firebase.firestore().doc('fooCollection/3HUA9W19DOf1yHdOM0DL'),
+    db.doc('fooCollection/3HUA9W19DOf1yHdOM0DL'),
+    {
+      snapshotListenOptions: { includeMetadataChanges: true },
+    }
+  );
+
+  useEffect(() => {
+    if (error) {
+      console.error(error);
+    } else if (!loading && value) {
+      console.log(value.data());
+    } else {
+      console.log('loading');
+    }
+  }, [value, loading, error])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+        <Router>
+          <Class path="/class/:id" />
+          <About path="/about" />
+          <Contact path="/contact" />
+        </Router>
+      <Footer />
+      {value && <p>{JSON.stringify(value.data())}</p>}
     </div>
   );
 }
