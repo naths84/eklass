3c71eb70653082c11af83609d2bc996709a23a29
Yanis Berkani <yanis.berkani@epfl.ch>
2020-04-04 23:25:00 +0200

    access the first elem of the array question and display its first question text

--- src/components/Quiz/dataContent.js
+++ src/components/Quiz/dataContent.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 //import XMLViewer from './react-xml-viewer';
 //import xmlText from './equations_deuxieme_degre.xml'
-import jsonData from "./eq_sec_deg.json";
+import jsonData from "./equations_deuxieme-degre.json";
 import jsonTestData from "./test.json";
 console.log(jsonData);
 
 class DataContent extends Component {
   constructor() {
     super()
-    this.questions = jsonData.quiz.question[0];
+    this.question = jsonData.quiz.question[0];
   }
   render() {
     return (
@@ -16,7 +16,7 @@ class DataContent extends Component {
         <h1>Quiz</h1>
         <h1>{this.question.type}</h1>
         <p>{this.question.name.text}</p>
-        <div dangerouslySetInnerHTML={{ __html: this.question.questiontext.text }} />
+        <div dangerouslySetInnerHTML={{__html: this.question.questiontext.text}} />
       </div>
     )
   }
