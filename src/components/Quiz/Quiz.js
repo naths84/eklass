2b15fee28da6dfa5de32c9a9ce2f0dffcda7ee79
Pierre Motard <p.motard@outlook.com>
2020-04-04 15:44:29 +0200

    import xml file

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import XMLViewer from 'react-xml-viewer'
+//import xmlText from 'equations_deuxieme_degre.xml'
+
+
+const xml = '<hello>World</hello>'
 
 export default function Quiz() {
+
     return (
         <div>
-            
+            <h1>Quiz</h1>
         </div>
     )
 }
