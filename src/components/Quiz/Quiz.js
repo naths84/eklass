22978829e90fadbdfeb4be446f6d3c80e8ac3931
Cedric Jud <cedric@apt.no>
2020-04-04 22:34:53 +0200

    Fix display data

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,17 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 //import XMLViewer from './react-xml-viewer';
 //import xmlText from './equations_deuxieme_degre.xml'
-import dataContent from './dataContent'
+import DataContent from "./DataContent";
 
-
-const xml = '<hello>World</hello>'
+const xml = "<hello>World</hello>";
 
 const Quiz = () => {
-    return (
-        <div>
-            <h1>Quiz</h1>
-            <dataContent/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Quiz</h1>
+      <DataContent />
+    </div>
+  );
+};
 export default Quiz;
--- src/components/Quiz/dataContent.js
+++ src/components/Quiz/dataContent.js
@@ -1,23 +1,28 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 //import XMLViewer from './react-xml-viewer';
 //import xmlText from './equations_deuxieme_degre.xml'
-import jsonData from './eq_sec_deg.json'
-import jsonTestData from './test.json';
+import jsonData from "./eq_sec_deg.json";
+import jsonTestData from "./test.json";
+    console.log(jsonData);
 
-class dataContent extends Component{
-    render (){
-        return(
-            <div>
-                <h1>Quiz</h1>
-                    {jsonTestData.map((elem, i) => {
-                    return <h1>{elem.type}</h1>
-                }
-            )}
-                    
-        </div>
-        )
-
-    }
+class DataContent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Quiz</h1>
+        {jsonTestData.map((elem, i) => {
+          console.log(elem)
+          return (
+            <div key={`question-element-${i}`}>
+              <h1>{elem['-type']}</h1>
+              <p>{elem.name.text}</p> 
+              <div dangerouslySetInnerHTML={{__html: elem.questiontext.text}} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
-        
-export default dataContent;
+
+export default DataContent;
