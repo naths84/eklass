e6537fbd2a3fca3d238a177336c6f1269a14fa52
Pierre Motard <p.motard@outlook.com>
2020-04-04 22:38:06 +0200

    example mathjax latex formula

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,13 +1,18 @@
-import React from 'react'
-
+import React, { Component } from 'react'
+import MathJax from 'react-mathjax2'
 
 const xml = '<hello>World</hello>'
+const tex = `f(x) = \\int_{-\\infty}^\\infty\\hat f(\\xi)\\,e^{2 \\pi i \\xi x}\\,d\\xi`
+
 
 export default function Quiz() {
-
     return (
         <div>
-            <h1>Quiz</h1>
+            <MathJax.Context input='tex'>
+                <div>
+                    This is an inline math formula: <MathJax.Node inline> { `f(x) = \\int_{-\\infty}^\\infty\\hat f(\\xi)\\,e^{2 \\pi i \\xi x}\\,d\\xi` }</MathJax.Node>
+                </div>
+            </MathJax.Context>
         </div>
-    )
+    );
 }
