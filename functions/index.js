7c2b6d34374dde8672f7ca2dc8292a57eedbb70d
Cedric Jud <cedric@apt.no>
2020-04-04 17:28:58 +0200

    Create xml2json cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const parser = require('xml2json');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const cors = require('cors')({origin: true});
+
+admin.initializeApp(functions.config().firebase);
+
+let db = admin.firestore();
+
+exports.createXML = functions.https.onRequest(async (req, res) => {
+    const xml = req.body;
+
+    return cors(req, res, async () => {
+      try {
+        const jsonString = await parser.toJson(xml);
+        const json = await JSON.parse(jsonString);
+
+        await db.collection('questions').doc('bigString').set({jsonString: jsonString});
+
+        res.json(json);
+      } catch (error) {
+        res.send(error);
+      }
+    })
+  }
+);
+
+
+
