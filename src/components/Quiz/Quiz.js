63cb72f06e93debc52f838711472e1cb00204d13
saquand <43468672+saquand@users.noreply.github.com>
2020-04-04 23:22:48 +0200

    try

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -3,11 +3,27 @@ import React from 'react'
 
 const xml = '<hello>World</hello>'
 
-export default function Quiz() {
+/*const equations_deuxieme_degre={
+    question: "quel est le delta de ?",
+    answer: 3,
+    // etc
+}*/
 
-    return (
-        <div>
-            <h1>Quiz</h1>
-        </div>
-    )
+//<Quiz quiz={equations_deuxieme_degre} />
+
+class Quiz extends React.Component{
+   render(){
+       const question = this.props.equations_deuxieme_degre.text;
+       return(
+           <div className="Quiz">
+               <div className="Quest">
+                   {question}
+               </div>
+               <div className="Rep"></div>
+           </div>
+       );
+   }
 }
+
+export default Quiz;
+
