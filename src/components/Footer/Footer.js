f9959ef48c070c4d6c130e1c486cc60e24442997
clementpetit <petitc@student.chalmers.se>
2020-04-05 10:27:25 +0200

    small change for footer position

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,15 +4,15 @@ import { Link } from "@reach/router";
 import styles from "./footer.css"
 
 const ColumnLink = props => (
-    <li style={{ display: "inline-block", fontSize: "15px", marginInlineStart: "10px", marginInlineEnd: "10px"}}>
+    <li style={{ display: "inline-block", fontSize: "15px", marginInlineStart: "10px", marginInlineEnd: "10px", marginTop: "30px"}}>
       <Link to={props.to} >{props.children}</Link>
     </li>
 )
   
 const Footer = (props) => {
     const { t } = useTranslation();
-    return (
-        <div style={{ position: "absolute", bottom: "0", width: "100%"}}>
+    return (//style={{ position: "absolute", bottom: "0", width: "100%"}}
+        <div >
              <footer>
                 <ul>
                     <ColumnLink to="/about/">{t("about")}</ColumnLink>
