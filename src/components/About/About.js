e2967a7908cd841c7ed8ca63b76657f5135414e6
saquand <43468672+saquand@users.noreply.github.com>
2020-04-06 13:33:04 +0200

    traduction

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,20 +4,19 @@ import Vector from 'components/Vector/Vector';
 
 import styles from "./About.module.css";
 
+import { useTranslation } from "react-i18next";
+
 const About = (props) => {
+  const { t } = useTranslation();
   return (
     <div>
       <div align="left" className={styles.Vector}>
         <Vector type='logo' className={styles.logo} />
       </div>
       
-      <div class="jumbotron">
+      <div class="jumbotron" style={{padding: "30px"}}>
       <h1 style={{ textAlign: "left", marginLeft: "30px", marginBottom: "30px"}}>Presentation</h1>
-        <p class="text-left">The worldwide coronavirus crisis is a unique opportunity to propose a global platform for school girls and boys, students and teachers across the world to communicate and learn.
-          Our idea is to propose a platform focused on the learning of mathematics through practice. We want to provide an extensive program arranged by topics going through all school levels before university.
-          Given a selection of chosen topics and range of levels, the student will be able to randomly generate a chosen number of unique exercises and practice on them. She/he will then have access to detailed automated feedback about each exercise's resolution.
-          On the other hand, given a selection of chosen topics, a teacher will be able to generate a unique set of exercises for each of her/his students with automated solutions. In this way, a student can submit her/his solutions independently of her/his classmates, the set is automatically corrected, and the detailed solutions can be consulted afterwards by the student.
-          We are planning to first propose the platform in English, French, Italian, Portuguese, Spanish, and possibly Asian languages. The subject is restricted to mathematics at a primary and secondary level, but could possibly be adapted to other subjects and higher levels.</p>
+       <p class="text-left">{t("description_about")}</p>
       </div>
     </div>
   )
--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -4,22 +4,21 @@ import styles from "./Team.module.css";
 import { useTranslation } from "react-i18next";
 
 const About = (props) => {
-    const { t, i18n } = useTranslation();
+    const { t } = useTranslation();
 
   return (      
       <div>
-        <h1 style={{ textAlign: "center", marginBottom: "30px"}}>{t("team")}</h1>
-        <h5>We are team of mathematicians and web developers, teachers and students:</h5>
-        <ul style={{ textAlign: "left"}}>
-            <li>Cedric Jud is a self-taught web developer based in Oslo.</li>
-            <li>Claude Marion got a PhD in Mathematics from Imperial College and has been teaching at universities in UK, Switzerland, Israel, Italy and Portugal. He inclusively designed with Prof. Testerman the three EPFL MOOC courses on linear algebra.</li>
-            <li>Nathan Scheinmann has a PhD in Mathematics from EPFL. He is currently teaching mathematics at CO des Coudriers in Geneva.</li>
-            <li>Peter Simko has a Masters degree in Mathematics from EPFL and currently teaches at Gymnase de Renens. He likes to read, write and code in his free time.</li>
-            <li>Clément Petit is currently a 3rd year student in Bachelor of Computer Science at EPFL.</li>
-            <li>Elia Saquand is a 3rd year student in Bachelor of Communication Systems at EPFL.</li>
-            <li>Pierre Motard is a 3rd year student in Bachelor of Communication Systems at EPFL.</li>
-            <li>Yanis Berkani is a 3rd year student in Bachelor of Computer Science at EPFL.</li>
-            
+        <h1 style={{ textAlign: "center", margin: "20px 0"}}>{t("team")}</h1>
+        <h5 style={{ color: "#44a49c"}}>{t("Presentation")}</h5>
+        <ul style={{ textAlign: "center", listStyle:"none"}}>
+            <li>{t("L1")}</li>
+            <li>{t("L2")}</li>
+            <li>{t("L3")}</li>
+            <li>{t("L4")}</li>
+            <li>{t("L5")}</li>
+            <li>{t("L6")}</li>
+            <li>{t("L7")}</li>
+            <li>{t("L8")}</li>   
         </ul>
 
 
